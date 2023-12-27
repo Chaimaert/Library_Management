a71fbb2820d339d5c28e2534396d765c8b5016e7
chaimaert <chaima.Rouita@emsi-edu.ma>
2023-12-27 21:01:44 +0100

    Update

--- Mindful_Library/Site1.Master.designer.cs
+++ Mindful_Library/Site1.Master.designer.cs
@@ -87,15 +87,6 @@ namespace Mindful_Library
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
 
         /// <summary>
-        /// LinkButton6 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton LinkButton6;
-
-        /// <summary>
         /// LinkButton11 control.
         /// </summary>
         /// <remarks>
