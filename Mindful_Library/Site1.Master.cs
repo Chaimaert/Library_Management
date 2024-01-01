eed49bcabaca16e5d53b28c972ea67d09e95d17f
chaimaert <chaima.Rouita@emsi-edu.ma>
2024-01-01 19:47:12 +0100

    Update
    buttons visibility

--- Mindful_Library/Site1.Master.cs
+++ Mindful_Library/Site1.Master.cs
@@ -28,6 +28,7 @@ namespace Mindful_Library
 
                     LinkButton3.Visible = true; //logout
                     LinkButton7.Visible = true; //hello_user
+                    LinkButton7.Text = "Hello" + Session["username"].ToString();
                 }
 
             }
